d360cf458f85c57a3cddde92a2bb2b3fae0e3228
seongwon <87584033+Magiof@users.noreply.github.com>
2022-05-01 16:25:37 +0900

    ADD: ORM config

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,16 +7,7 @@ import { ArticlesModule } from './articles/articles.module';
 @Module({
   imports: [
     //ref: https://docs.nestjs.com/techniques/database
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: 'root',
-      database: 'hhet99',
-      entities: [],
-      synchronize: true,
-    }),
+    TypeOrmModule.forRoot(),
     ArticlesModule,
   ],
   controllers: [AppController],
