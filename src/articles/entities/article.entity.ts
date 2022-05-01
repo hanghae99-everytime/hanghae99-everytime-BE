cd73036e4e2085fe6641a69389178ed0ff27883d
seongwon <87584033+Magiof@users.noreply.github.com>
2022-05-01 20:27:43 +0900

    FIX: for typeORM

--- src/articles/entities/article.entity.ts
+++ src/articles/entities/article.entity.ts
@@ -1,7 +1,19 @@
-export class article {
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Article {
+  @PrimaryGeneratedColumn()
   id: number;
+
+  @Column()
   userId: number;
+
+  @Column()
   title: string;
+
+  @Column()
   content: string;
+
+  @Column()
   images: string[];
 }
