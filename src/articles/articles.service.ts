6fff14fec0839135542dcf7e7d44cfdbb18cb56e
seongwon <87584033+Magiof@users.noreply.github.com>
2022-05-01 14:43:47 +0900

    ADD: articles.service

---
+++ src/articles/articles.service.ts
@@ -0,0 +1,7 @@
+import { Injectable } from '@nestjs/common';
+import {} from './dto/create-article.dto';
+import {} from './dto/update-article.dto';
+import { article } from './entities/article.entity';
+
+@Injectable()
+export class ArticlesService {}
