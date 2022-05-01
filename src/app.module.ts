629287a603169b7eedc6f90ec9c27fbc14458766
seongwon <87584033+Magiof@users.noreply.github.com>
2022-05-01 15:03:24 +0900

    ADD: typeorm settings in module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,24 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ArticlesModule } from './articles/articles.module';
 
 @Module({
-  imports: [ArticlesModule],
+  imports: [
+    //ref: https://docs.nestjs.com/techniques/database
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'root',
+      database: 'hhet99',
+      entities: [],
+      synchronize: true,
+    }),
+    ArticlesModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
