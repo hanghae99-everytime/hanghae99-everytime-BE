a9f6c28ee9b58b119f7d55487145f5e44f9debff
seongwon <87584033+Magiof@users.noreply.github.com>
2022-05-01 21:03:37 +0900

    ADD: users

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,14 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ArticlesModule } from './articles/articles.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
     //ref: https://docs.nestjs.com/techniques/database
     TypeOrmModule.forRoot(),
     ArticlesModule,
+    UsersModule,
   ],
   controllers: [AppController],
   providers: [AppService],
