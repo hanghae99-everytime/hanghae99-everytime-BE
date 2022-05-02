c60ab4da35af0fd0919a045ef80abcf68d258956
seongwon <87584033+Magiof@users.noreply.github.com>
2022-05-02 21:23:36 +0900

    ADD: DB password

--- ormconfig.js
+++ ormconfig.js
@@ -3,6 +3,7 @@ module.exports = {
   host: process.env.DATABASE_HOST,
   port: process.env.DATABASE_PORT,
   username: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
   database: process.env.DATABASE,
   entities: ['dist/**/*.entity{.ts,.js}'],
   synchronize: true,
