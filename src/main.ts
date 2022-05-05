144f4a6c565b56beeebec80afcb591592c228150
seongwon <87584033+Magiof@users.noreply.github.com>
2022-05-05 16:46:07 +0900

    ADD: 틀만짰음.. 회의필요

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,7 @@ async function bootstrap() {
       transform: true,
     }),
   );
-  await app.listen(parseInt(process.env.PORT, 10));
+  await app.listen(process.env.PORT);
 }
 bootstrap();
 //test
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { User } from './entities/user.entity';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  private users: User[] = [];
+
+  // async signUp(CreateUserDto): Promise<any> {
+  //   const isExist = await this.userRepository.findOne({
+  //     email: CreateUserDto.email,
+  //   });
+  // }
+}
